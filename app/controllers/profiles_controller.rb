7a374d08971733a090e99c29fa001b776bb8d0eb
Eduardo <egbulsoni@gmail.com>
2019-04-26 11:20:40 -0300

    created editing functionality

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,14 +18,33 @@ class ProfilesController < ApplicationController
     # byebug
 
   end
+
   def show
     @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @values = Scraper.scrape(edit_params[:url])
+    @profile = Profile.find(params[:id])
+    @profile.update({name: edit_params[:name],
+                    image: @values[:img], username: edit_params[:username],
+                    url: @values[:url], desc: edit_params[:desc]})
+
+    redirect_to profile_path(@profile)
   end
 
   def search
   end
 
   private
+
+    def edit_params
+      params.require(:profile).permit(:name, :url, :image, :username, :desc)
+    end
 
     def allowed_params
       params.require(:profile).permit(:name, :url)
