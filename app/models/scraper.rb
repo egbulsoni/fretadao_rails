bd373b8b0368add750d04e8e6aaef4a0bd861b57
Eduardo <egbulsoni@gmail.com>
2019-04-27 19:18:40 -0300

    added navbar

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -17,6 +17,7 @@ module Scraper
     url = ""
     url = start unless page_query.start_with?(start)
     url << page_query if page_query.match(twitter)
+    url << "twitter.com/#{page_query[1..]}" if page_query[0] = '@'
     if Embiggen::URI(page_query.to_s).shortened?
       url = Embiggen::URI(page_query.to_s).expand.to_s
     end
@@ -29,12 +30,11 @@ module Scraper
     profile_desc = parsed_page.css('p.ProfileHeaderCard-bio.u-dir')
 
     # return a hash (dictionary) with profile info
-    profile = {
+    {
       img: ShortURL.shorten(profile_pic.to_s).to_s,
       username: profile_nickname.text.strip.gsub('\n', ''),
       url: ShortURL.shorten(url.to_s).to_s,
       desc: profile_desc.text.strip.to_s
     }
-    profile
   end
 end
